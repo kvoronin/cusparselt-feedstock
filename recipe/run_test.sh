72aaa42b3ba3f4f9588d33e0f7ae01976049ece2
Leo Fang <leof@nvidia.com>
2023-05-03 23:19:52 -0400

    Update run_test.sh

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,7 +6,8 @@ test -f $PREFIX/lib/libcusparseLt.so
 ${GCC} test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
 ./test_load_elf $PREFIX/lib/libcusparseLt.so
 
-git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
-cd sample_linux/cuSPARSELt/spmma
-error_log=$(nvcc -I$PREFIX/include -L$PREFIX/lib -lcusparse -lcusparseLt spmma_example.cpp -o spmma_example 2>&1)
-echo $error_log
+# temporarily disable compilation tests, as the samples for v0.4.0 are not up yet
+#git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
+#cd sample_linux/cuSPARSELt/spmma
+#error_log=$(nvcc -I$PREFIX/include -L$PREFIX/lib -lcusparse -lcusparseLt spmma_example.cpp -o spmma_example 2>&1)
+#echo $error_log

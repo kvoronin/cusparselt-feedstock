40a7cb80de50fd5e1ead220f6cb646d1ce575bc0
Leo Fang <leof@nvidia.com>
2023-08-30 15:13:06 +0800

    fix header path on linux

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -9,7 +9,14 @@ if [[ $target_platform == "linux64" ]]; then
     ./test_load_elf $PREFIX/lib/libcusparseLt.so
 fi
 
+# for cuSPARSE header
+if [[ "${cuda_compiler_version}" =~ 12.* ]]; then
+    CUDA_INC_PATH="${PREFIX}"
+elif [[ "${cuda_compiler_version}" =~ 11.* ]]; then
+    CUDA_INC_PATH="${CUDA_PATH}"
+fi
+
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuSPARSELt/matmul
-error_log=$(nvcc --std=c++14 -I$PREFIX/include -L$PREFIX/lib -lcusparse -lcusparseLt -lnvrtc matmul_example.cpp -o matmul_example 2>&1)
+error_log=$(nvcc --std=c++14 -I$CUDA_INC_PATH -I$PREFIX/include -L$PREFIX/lib -lcusparse -lcusparseLt -lnvrtc matmul_example.cpp -o matmul_example 2>&1)
 echo $error_log

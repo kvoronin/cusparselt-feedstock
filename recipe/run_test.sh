8091beffca0ac6366a4315dbd06a4a3f9cab2a83
Leo Fang <leof@nvidia.com>
2023-08-30 14:42:04 +0800

    skip dlopen test on aarch64/ppc64le

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,8 +3,11 @@ set -ex
 
 test -f $PREFIX/include/cusparseLt.h
 test -f $PREFIX/lib/libcusparseLt.so
-${GCC} test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
-./test_load_elf $PREFIX/lib/libcusparseLt.so
+# skip symbol tests on non-x86 archs due to the usage of newer glibc symbols
+if [[ $target_platform == "linux64" ]]; then
+    ${GCC} test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
+    ./test_load_elf $PREFIX/lib/libcusparseLt.so
+fi
 
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuSPARSELt/spmma

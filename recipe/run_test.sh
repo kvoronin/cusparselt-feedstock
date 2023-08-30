11a3c38429d823cfed90ad17fe24f1aa53f91bff
Leo Fang <leof@nvidia.com>
2023-08-30 14:38:04 +0800

    restore compilation tests on linux

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,8 +6,7 @@ test -f $PREFIX/lib/libcusparseLt.so
 ${GCC} test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
 ./test_load_elf $PREFIX/lib/libcusparseLt.so
 
-# temporarily disable compilation tests, as the samples for v0.4.0 are not up yet
-#git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
-#cd sample_linux/cuSPARSELt/spmma
-#error_log=$(nvcc -I$PREFIX/include -L$PREFIX/lib -lcusparse -lcusparseLt spmma_example.cpp -o spmma_example 2>&1)
-#echo $error_log
+git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
+cd sample_linux/cuSPARSELt/spmma
+error_log=$(nvcc -I$PREFIX/include -L$PREFIX/lib -lcusparse -lcusparseLt spmma_example.cpp -o spmma_example 2>&1)
+echo $error_log

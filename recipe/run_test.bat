389a65e8733e37255bfe17a054025bdb75d5c1cf
Leo Fang <leof@nvidia.com>
2023-08-30 16:35:25 +0800

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -6,5 +6,5 @@ if not exist %LIBRARY_BIN%\\cusparseLt.dll exit 1
 if not exist %LIBRARY_LIB%\\cusparseLt.lib exit 1
 
 git clone "https://github.com/NVIDIA/CUDALibrarySamples.git" sample_linux
-cd sample_linux\cuSPARSELt\spmma
-call nvcc -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcusparse -lcusparseLt spmma_example.cpp -o spmma_example
+cd sample_linux\cuSPARSELt\matmul
+call nvcc --std=c++14 -I%LIBRARY_INC% -L%LIBRARY_LIB% -lcusparse -lcusparseLt -lnvrtc matmul_example.cpp -o matmul_example

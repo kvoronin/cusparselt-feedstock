6be339427a5a9e9b8b5c87ceef12f300dd8aa203
Matthew Jones <13078940+mtjrider@users.noreply.github.com>
2024-03-11 13:45:38 -0600

    PRELOAD libcuda.so
    Adding libcuda.so as a PRE_LOAD for test_load_elf
    Adding $PREFIX/lib/stubs to library link paths along with -lcuda

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,8 +6,10 @@ test -f $PREFIX/lib/libcusparseLt.so
 
 # skip symbol/compilation tests on non-x86 archs due to the usage of newer glibc symbols
 if [[ $target_platform == "linux-64" ]]; then
+    export CUDA_STUB="$PREFIX/lib/stubs/libcuda.so"
+    
     ${GCC} test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
-    ./test_load_elf $PREFIX/lib/libcusparseLt.so
+    LD_PRELOAD="$CUDA_STUB" ./test_load_elf $PREFIX/lib/libcusparseLt.so
 
     # for cuSPARSE header
     if [[ "${cuda_compiler_version}" =~ 12.* ]]; then
@@ -18,6 +20,6 @@ if [[ $target_platform == "linux-64" ]]; then
 
     git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
     cd sample_linux/cuSPARSELt/matmul
-    error_log=$(nvcc --std=c++14 -I$CUDA_INC_PATH -I$PREFIX/include -L$PREFIX/lib -lcusparse -lcusparseLt -lnvrtc matmul_example.cpp -o matmul_example 2>&1)
+    error_log=$(nvcc --std=c++14 -I$CUDA_INC_PATH -I$PREFIX/include -L$PREFIX/lib -L$PREFIX/lib/stubs -lcusparse -lcusparseLt -lcuda -lnvrtc matmul_example.cpp -o matmul_example 2>&1)
     echo $error_log
 fi

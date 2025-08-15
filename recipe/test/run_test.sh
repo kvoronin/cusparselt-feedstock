594e9bd9e7d20514bee9f8f626d73c03b4182531
Kirill Voronin <kvoronin@nvidia.com>
2025-08-14 23:09:10 -0700

    updating the recipe for 0.8.0 and also enabling CTK 12 and CTK 13 (with a WAR)

--- recipe/test/run_test.sh
+++ recipe/test/run_test.sh
@@ -7,13 +7,9 @@ ${GCC} test/test_load_elf.c -std=c99 -Werror -ldl -o test_load_elf
 LD_PRELOAD="$CUDA_STUB" ./test_load_elf $PREFIX/lib/libcusparseLt.so
 
 # for cuSPARSE header
-if [[ "${cuda_compiler_version}" =~ 12.* ]]; then
-    CUDA_INC_PATH="${PREFIX}/include"
-elif [[ "${cuda_compiler_version}" =~ 11.* ]]; then
-    CUDA_INC_PATH="${CUDA_PATH}/include"
-fi
+export CUDA_INC_PATH="${CUDA_PATH}/include"
 
 git clone https://github.com/NVIDIA/CUDALibrarySamples.git sample_linux/
 cd sample_linux/cuSPARSELt/matmul
-error_log=$(nvcc --std=c++14 -I$CUDA_INC_PATH -I$PREFIX/include -L$PREFIX/lib -L$PREFIX/lib/stubs -lcusparse -lcusparseLt -lcuda -lnvrtc matmul_example.cpp -o matmul_example 2>&1)
+error_log=$(nvcc --std=c++14 -I$CUDA_INC_PATH -I$PREFIX/include -L$PREFIX/lib -L$PREFIX/lib/stubs -lcusparse -lcusparseLt -lcuda -lnvrtc matmul_example.cpp -o matmul_example  2>&1)
 echo $error_log
